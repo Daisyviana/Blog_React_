1b6f356a98fdb831433cceb8a15093d56965ada9
Jeanine <jeanine.a.santiago@gmail.com>
2023-05-18 19:10:09 -0300

    link

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,25 @@ import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/login/Login';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 
 
 function App() {
   return (
     <>
-      <Navbar />
-       <Home />
-      <Footer />
+    <BrowserRouter>
+        <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />}/>
+            <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
+
+          </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
